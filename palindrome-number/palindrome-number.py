a599d6742eb1d47508933dfd03c4424cfe21dcf1
Jack Quinn <quinny2727@gmail.com>
2021-01-20 19:22:40 -0500

    Time: 68 ms (48.09%), Space: 14.2 MB (79.81%) - LeetHub

---
+++ palindrome-number/palindrome-number.py
@@ -0,0 +1,15 @@
+import math
+​
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        
+        # reverted last part of the number
+        z = 0
+        while (x > z):
+            z = z * 10 +  x % 10
+            x = math.floor(x / 10)
+        
+        # for an odd number we can just skip the central digit
+        return x == z or x == math.floor(z / 10)
